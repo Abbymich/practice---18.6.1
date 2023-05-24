d07cf5e664e42f42a6b4da51914d5ec1da15b2c7
Abbymich <eka.magomedkerimova@yahoo.com>
2023-05-25 00:21:46 +0500

    Add files

---
+++ config.py
@@ -0,0 +1,10 @@
+TOKEN = '6052027458:AAGFbnGquPOmxi98nLuZ6-tgJIW_7PYHR8c'
+
+keys = {
+    'Рубль': 'RUB',
+    'Евро': 'EUR',
+    'Доллар': 'USD',
+    'рубль': 'RUB',
+    'евро': 'EUR',
+    'доллар': 'USD',
+}
